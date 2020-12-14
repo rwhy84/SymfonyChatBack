2864f73d4cdfa9f08a3526e4ef7b75af045dbcb4
Renaud Bordet <rbordet84@gmail.com>
2020-12-14 13:57:13 +0100

    Symgfony Chat Back

---
+++ src/Entity/ChatMessage.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ChatMessageRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ORM\Entity(repositoryClass=ChatMessageRepository::class)
+ * @ApiResource(
+ * attributes={"order"={"id": "DESC"}}
+ * )
+ */
+class ChatMessage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
